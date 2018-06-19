cc66cc361c118fd19fbc87c457d805d7ffdd77a6
Сергей Гонимар <sergey.gonimar@gmail.com>
2018-06-20 01:23:08 +0500

    basic tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL);
+
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_DEBUG', true);
+
+$_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+Yii::setAlias('@robokassa/tests/unit', __DIR__ . '/unit');
+Yii::setAlias('@robokassa', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
