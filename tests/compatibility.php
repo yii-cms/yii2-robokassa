5eb33574a20acadc7c71a8fa1a9ac165e1abf481
Sergey Gonimar <sergey.gonimar@gmail.com>
2019-06-25 11:03:44 +0500

    fix phpunit version

--- tests/compatibility.php
+++ tests/compatibility.php
@@ -3,7 +3,6 @@
 /*
  * Ensures compatibility with PHPUnit < 6.x
  */
-
 namespace PHPUnit\Framework\Constraint {
     if (!class_exists('PHPUnit\Framework\Constraint\Constraint') && class_exists('PHPUnit_Framework_Constraint')) {
         abstract class Constraint extends \PHPUnit_Framework_Constraint
@@ -11,7 +10,20 @@ namespace PHPUnit\Framework\Constraint {
         }
     }
 }
-
+namespace PHPUnit\TextUI {
+    if (!class_exists('\PHPUnit\TextUI\ResultPrinter') && class_exists('PHPUnit_TextUI_ResultPrinter')) {
+        class ResultPrinter extends \PHPUnit_TextUI_ResultPrinter
+        {
+        }
+    }
+}
+namespace PHPUnit\Framework\Error {
+    if (!class_exists('PHPUnit\Framework\Error\Notice') && class_exists('PHPUnit_Framework_Error_Notice')) {
+        class Notice extends \PHPUnit_Framework_Error_Notice
+        {
+        }
+    }
+}
 namespace PHPUnit\Framework {
     if (!class_exists('PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase')) {
         abstract class TestCase extends \PHPUnit_Framework_TestCase
@@ -23,7 +35,6 @@ namespace PHPUnit\Framework {
             {
                 $this->setExpectedException($exception);
             }
-
             /**
              * @param string $message
              */
@@ -36,7 +47,6 @@ namespace PHPUnit\Framework {
                 }
                 $this->setExpectedException($this->getExpectedException(), $message);
             }
-
             /**
              * @param string $messageRegExp
              */
