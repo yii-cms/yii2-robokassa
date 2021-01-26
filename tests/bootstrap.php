ff35adbf32c12837822dda0474159fe3ef86341c
Sergey Gonimar <sergey.gonimar@gmail.com>
2021-01-26 21:25:22 +0500

    actions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,5 +11,6 @@ $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+Yii::setAlias('@robokassa/tests', __DIR__);
 Yii::setAlias('@robokassa/tests/unit', __DIR__ . '/unit');
 Yii::setAlias('@robokassa', dirname(__DIR__) . '/src');
